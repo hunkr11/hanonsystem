495566a63039b007fec6df6cd4b9dda35701bfed
hunkr11 <hunkr11@gmail.com>
2018-04-28 15:01:05 +0530

    Initial commit

---
+++ src/main/java/com/hanon/system/hanonsystem/HanonsystemApplication.java
@@ -0,0 +1,12 @@
+package com.hanon.system.hanonsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanonsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanonsystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hanon/system/hanonsystem/HanonsystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hanon.system.hanonsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HanonsystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
